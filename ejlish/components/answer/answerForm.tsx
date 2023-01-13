955f722759f23576fcfe2d4e5b675aab81e5f224
윤은재 <ejyoon@unopnd.com>
2023-01-13 19:13:33 +0900

    Test

--- ejlish/components/answer/answerForm.tsx
+++ ejlish/components/answer/answerForm.tsx
@@ -29,7 +29,7 @@ export default function answerForm(){
 
   const setBestscore = async () => {
     const response = await axios.get(process.env.NEXT_PUBLIC_SERVER+'/bestscore');
-    console.log("resIIII",response.data.data.best_score);
+    console.log("resIIII",response.data.data);
     setBest(12);
   }
 
